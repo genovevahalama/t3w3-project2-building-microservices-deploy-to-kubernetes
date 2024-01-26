8b2ad938cff80affb6d526b2157a5f3ce1b60868
Genoveva  Halama <genoveva.halama@telekom.de>
2024-01-26 09:32:32 +0100

    added spells json einto mongodb

--- mongodb_loader/datafetch_load_mongodb.py
+++ mongodb_loader/datafetch_load_mongodb.py
@@ -2,8 +2,10 @@ import pymongo
 import json
 import requests
 
-def fetch_jsondata():
-  url = "https://hp-api.onrender.com/api/characters/staff"
+spells_url="https://hp-api.onrender.com/api/spells"
+stuff_url="https://hp-api.onrender.com/api/characters/staff"
+
+def fetch_jsondata(url):
   response = requests.get(url)
   if response.status_code == 200:
           #print(response.json())
@@ -12,9 +14,10 @@ def fetch_jsondata():
   else:
           return "Error: " + str(response.status_code)
 
-json_hogwarts_stuff = fetch_jsondata()
-print(type(json_hogwarts_stuff))
-print(json_hogwarts_stuff)
+list_stuff = fetch_jsondata(stuff_url)
+list_spells = fetch_jsondata(spells_url)
+print("stuff: ", list_stuff)
+print("spells: ",list_spells)
 
 # MongoDB connection string Format: mongodb://username:password@host:port/database
 connection_string = "mongodb://mongodb-container:27017/testdb"
@@ -27,24 +30,20 @@ except Exception as e:
     print("Error connecting to MongoDB:", e)
 
 # Select your database
-db = client.characters
+db = client['hogwarts']
 
-# Select the collection
-collection = db.characterscollection
+db['stuff'].insert_many(list_stuff)
+db['spells'].insert_many(list_spells)
 
-# JSON data
-json_data =json_hogwarts_stuff
-print("JSON data to be stored: ",json_data)
+def print_collection(collection_name):
+    collection = db[collection_name]
+    for doc in collection.find():
+        print(doc)
 
-# Insert JSON data into MongoDB
-if isinstance(json_data, list):
-    collection.insert_many(json_data)
-elif isinstance(json_data, dict):
-    collection.insert_one(json_data)
-else:
-    print("Error: json_data is neither a list nor a dictionary")
+print("Stuff Collection:")
+print_collection('stuff')
 
-print("JSON data inserted successfully")
+print("\nSpells Collection:")
+print_collection('spells')
 
-for doc in collection.find():
-    print(doc)
+client.close()
